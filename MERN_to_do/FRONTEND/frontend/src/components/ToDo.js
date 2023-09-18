eb218fe7eb86f3ca433568bfa21063b0db1c21e9
NegiShweta <shwetanegi0066@gmail.com>
2023-09-18 16:24:42 +0530

    test

---
+++ MERN_to_do/FRONTEND/frontend/src/components/ToDo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ToDo = ({text,updateMode, Delete_Mode}) => {
+  return (
+    <div className='ToDo'>
+       <div className='text'>{text}</div> 
+       </div>
+
+    
+
+  )
+
+}
+
+export default ToDo
